306f987f6c2d57e39465856366dfbe54c8943c92
Mihailghrgh <mihailghrgh@gmail.com>
2025-07-26 20:55:33 +0300

    19.2 - Custom Actions - Javasript

--- .github/actions/js-deps-update/index.js
+++ .github/actions/js-deps-update/index.js
@@ -1,9 +1,24 @@
-
-const core = require('@actions/core');
-
+const core = require("@actions/core");
 
 async function run() {
-    core.info('Starting the js-deps-update action ///');
+  /*
+    This actions is used to update the npm dependencies in the package.json Yay
+
+    1. base branch frp, which to check for updates
+
+    2. target-branch to use to create the PR
+
+    3. Github token to use for authentication (to authorize PR creation)
+
+    4. Working directory of which to update the dependencies updates
+
+    5.Check if there are modified updates ,IF THERE ARE modified files create a PR request using the target branch.
+
+    6. Create a PR to base-branch using the octokit API
+
+    6. Otherwise , conclude the custom action
+    */
+  core.info("Starting the js-deps-update action ///");
 }
 
-run()
+run();
